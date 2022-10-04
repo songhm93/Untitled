f21b772dd5dd37d00da8c60fdb576021400c4222
homin <hm2042@naver.com>
2022-10-04 14:54:27 +0900

    add baseweapon

---
+++ Source/Melee/ToughSword.cpp
@@ -0,0 +1,2 @@
+#include "ToughSword.h"
+
---
+++ Source/Melee/ToughSword.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseWeapon.h"
+#include "ToughSword.generated.h"
+
+
+UCLASS()
+class MELEE_API AToughSword : public ABaseWeapon
+{
+	GENERATED_BODY()
+	
+};
