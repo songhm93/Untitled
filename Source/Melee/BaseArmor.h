5601165cab7bb728f4da0fe2bb0777af607ccb50
homin <hm2042@naver.com>
2022-10-10 15:08:17 +0900

    Add BaseArmor Class

---
+++ Source/Melee/BaseArmor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseEquippable.h"
+#include "BaseArmor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MELEE_API ABaseArmor : public ABaseEquippable
+{
+	GENERATED_BODY()
+	
+};
